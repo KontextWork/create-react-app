cfdd0dc8f0b6e87fe6c2a1d6f75eaf721c281284
jp <jan.pretzel@kontextwork.de>
2020-01-20 11:28:09 +0100

    Change build output
    Squashed commits:
    - Do not inline the runtime chunk in production
    - Do not hide js output in static/js
    - Do not copy the public folder for production builds
    - Move all chunks to build/dist
    This makes it easier to extract in CI/CD.
    - Provide a single main.js file
    This file contains our app bootstrap, the webpack runtime and all vendors
    required by our bootstrap/entrypoint.
    - Do not pack build into dist subfolder

--- packages/react-scripts/scripts/build.js
+++ packages/react-scripts/scripts/build.js
@@ -75,8 +75,6 @@ checkBrowsers(paths.appPath, isInteractive)
     // Remove all content but keep the directory so that
     // if you're in it, you don't end up in Trash
     fs.emptyDirSync(paths.appBuild);
-    // Merge with the public folder
-    copyPublicFolder();
     // Start the webpack build
     return build(previousFileSizes);
   })
@@ -216,12 +214,5 @@ function build(previousFileSizes) {
         warnings: messages.warnings,
       });
     });
-  });
-}
-
-function copyPublicFolder() {
-  fs.copySync(paths.appPublic, paths.appBuild, {
-    dereference: true,
-    filter: file => file !== paths.appHtml,
   });
 }
